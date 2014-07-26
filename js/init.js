00c2dfe65131d7dcee462cca0a84a55c452c362d
Obulapathi N Challa <obulpathi@gmail.com>
2014-07-26 14:57:25 -0400

    Initial modifications

--- js/init.js
+++ js/init.js
@@ -32,17 +32,17 @@ $(function () {
 			isToggled = true;
 		}
 	});
-	
+
 	/* ---------------------------------------------------------
 	 * WOW
 	 */
-	
+
 	new WOW().init();
-	
+
 	/* ---------------------------------------------------------
 	 * Scroll arrow
 	 */
-	
+
 	$("#scroll").click(function () {
 	 	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 	 		var target = $(this.hash);
@@ -67,7 +67,7 @@ $(function () {
 		minutes: "minutes",
 		seconds: "seconds"
 	};
-	
+
 	// year/month/day
 	$('#countdown').countdown('2015/6/12', function (event) {
 		$(this).html(event.strftime(
@@ -96,15 +96,15 @@ $(function () {
 		submitHandler: function (validator, form, submitButton) {
 			var l = Ladda.create(submitButton[0]),
 				btnText = submitButton.children(".ladda-label");
-			
+
 			l.start();
 			btnText.html("Signing up...");
-			
-			$.get(form.attr('action'), form.serialize(), function(result) { 
-				btnText.html(result.message);							
+
+			$.get(form.attr('action'), form.serialize(), function(result) {
+				btnText.html(result.message);
 			}, 'json')
-			.always(function() { 
-				l.stop(); 
+			.always(function() {
+				l.stop();
 				validator.disableSubmitButtons(true);
 			});
 		},
@@ -168,10 +168,10 @@ $(function () {
 		submitHandler: function (validator, form, submitButton) {
 			var l = Ladda.create(submitButton[0]),
 				btnText = submitButton.children(".ladda-label");
-			
+
 			l.start();
 			btnText.html("Sending...");
-			
+
 			$.post(form.attr('action'), form.serialize(), function(result) {
 				if(result.sent){
 					btnText.html("Sent!");
@@ -179,17 +179,17 @@ $(function () {
 				else{
 					btnText.html("Error!");
 				}
-				
+
 				// Reset form after 5s
 				setTimeout(function() {
 					btnText.html("Submit");
 					$(form[0])[0].reset();
 					validator.resetForm();
 				}, 5000);
-				
+
 			}, 'json')
-			.always(function() { 
-				l.stop(); 
+			.always(function() {
+				l.stop();
 				validator.disableSubmitButtons(true);
 			});
 		},
